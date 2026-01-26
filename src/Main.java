6847964608feb79bb0c7dfb686a39e98cd4440ba
PrikolistTheSillyThing <uzun.andrei13@gmail.com>
2026-01-26 13:02:16 +0200

    file created

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I'm thinking of a number between 1 and 100.");
+
+    }
+}

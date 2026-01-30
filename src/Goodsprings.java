0b8b19bb71961c90b14345787a4c24e608ba5009
unknown <uzun.andrei13@gmail.com>
2026-01-30 23:42:45 +0200

    changed the idea, two npcs fighting instead, created an abstract class for that

--- src/Goodsprings.java
+++ src/Goodsprings.java
@@ -1,33 +1,70 @@
-abstract class Resident {
-    protected String name;
-    protected int age;
-    protected String gender;
-    protected String profession;
-    protected String weapon;
+import java.util.Random;
 
-    protected Resident(String name, int age, String gender, String profession, String weapon){
+abstract class NPC {
+    private final String name;
+    private final float maxHp;
+    private final float currentHp;
+    private final String weapon;
+    private final String affiliation;
+
+    protected NPC(String name, int maxHp, String weapon, String affiliation){
         this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.profession = profession;
+        this.maxHp = maxHp;
+        this.currentHp = maxHp;
         this.weapon = weapon;
+        this.affiliation = affiliation;
+        System.out.println(name + " arrives to town.");
+        if (affiliation.equalsIgnoreCase("Goodsprings")) {
+            System.out.println("That is a well-known resident of Goodsprings.");
+        } else System.out.println("That is no local...");
     }
 
-    public abstract void relax();
-    public abstract void work();
+    public abstract void idle();
+    public abstract void attack(NPC npc);
+    public abstract void takeDamage();
+
+    public String getName() {
+        return name;
+    }
+
+    public float getCurrentHp() {
+        return currentHp;
+    }
 }
 
-class EasyPete extends Resident {
-    public EasyPete(String name, int age, String gender, String profession, String weapon) {
-        super("Easy Pete",68, "Male", "Former prospector", "Dynamite");
+class EasyPete extends NPC {
+    public EasyPete() {
+        super("Easy Pete", 135, "dynamite", "Goodsprings");
     }
     @Override
-    public void relax(){
-        System.out.println(name + " is taking it easy outside the Prospector Saloon.");
+    public void idle(){
+        System.out.println(getName() + " is taking it easy outside the Prospector Saloon.");
     }
+
     @Override
-    public void work(){
-        System.out.println(name + " is helping with Brahmins and Bighorners.");
+    public void attack(NPC npc){
+        System.out.println(getName() + " is throwing dynamite at " + npc.getName() + "!");
+        npc.takeDamage();
+    }
+
+    @Override
+    public void takeDamage() {
+        Random random = new Random();
+        int num;
+
+        if (random.nextBoolean()) {
+           num = 1;
+        } else {
+            num = 2;
+        }
+
+        // maaaaaybe return to protected...?
+        float hp = getCurrentHp();
+        if (num == 1) {
+            System.out.println(getName() + " has taken damage.");
+            hp -= 20;
+        }
+
     }
 }
 
